e7a2909f76caf92473228852222c7cc065311eaf
Muhammad K <akhalid.code@gmail.com>
2021-02-20 12:05:58 -0800

    setup react and intial files

--- clicky-x-men/src/App.js
+++ clicky-x-men/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Cards from "./components/cards";
+import Footer from "./components/footer";
+import Jumbotron from "./components/jumbotron";
+import Navbar from "./components/navbar";
+import Wrapper from "./components/wrapper";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
 }
 
 export default App;
