6a4203ab8220cbe55ef2cdaec6080c31d8073261
Muhammad K <akhalid.code@gmail.com>
2021-02-20 13:27:41 -0800

    add Footer component to display

--- clicky-x-men/src/App.js
+++ clicky-x-men/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
-import Cards from "./components/cards";
-import Footer from "./components/footer";
-import Jumbotron from "./components/jumbotron";
-import Navbar from "./components/navbar";
-import Wrapper from "./components/wrapper";
+import Wrapper from "./components/wrapper/wrapper";
+// import Cards from "./components/cards/cards";
+import Footer from "./components/footer/footer";
+// import Jumbotron from "./components/jumbotron/jumbotron";
+// import Navbar from "./components/navbar/navbar";
+// import xmen from "./xmen.json";
 
 class App extends Component {
 
+	//state
+
+	//functions
+
+	//render
+	render() {
+		return (
+			<Wrapper>
+				{/* <Navbar></Navbar> */}
+				{/* <Jumbotron></Jumbotron> */}
+				{/* Card Logic */}
+				<Footer></Footer>
+			</Wrapper>
+		);
+	}
 }
 
 export default App;
---
+++ clicky-x-men/src/components/footer/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./footer.css";
+
+function Footer() {
+	return (
+		<footer class="footer">
+			<div class="bottom">
+				"Clicky X-men! "
+				{/* <img alt="react" src="public/"></img> */}
+			</div>
+		</footer>
+	)
+}
+
+export default Footer;
