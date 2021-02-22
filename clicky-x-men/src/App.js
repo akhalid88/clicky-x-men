468bab2f3830700b68c2b06b381fc84b08a9f442
Muhammad K <akhalid.code@gmail.com>
2021-02-22 00:43:33 -0800

    remove unnecessary code; add score counters

--- clicky-x-men/src/App.js
+++ clicky-x-men/src/App.js
@@ -2,34 +2,55 @@ import React, { Component } from "react";
 import Wrapper from "./components/wrapper/wrapper";
 import Navbar from "./components/navbar/navbar";
 import Jumbotron from "./components/jumbotron/jumbotron";
-import Cards from "./components/cards/cards";
-import Footer from "./components/footer/footer";
-import xmen from "./xmen.json";
 import Card from "./components/cards/cards";
+import Footer from "./components/footer/footer";
+
+import xmen from "./xmen.json";
+
 
 class App extends Component {
 
 	//state
 	state = {
 		xmen: xmen,
-		playDeck: []
+		selectedCards: [],
+		currentScore: 0,
+		maxScore: 0
 	};
+
+	componentDidMount() {
+		this.shuffleCards();
+	}
 
 	// variables and functions
 	shuffleCards = () => {
-
 		const xmen = this.state.xmen.sort(() => Math.random() - 0.5);
 		this.setState({ xmen: xmen });
-		// console.log(this.state.playDeck);
 	}
 
-	selectCard = id => {
-		console.log(id);
+	selectCard = (id) => {
+
+
+
+		for (var i = 0; i < this.state.selectedCards.length; i++) {
+			if (id == this.state.selectedCards[i]) {
+				//display failure
+				//reload page
+				console.log("Hello");
+			} else {
+				this.setState({ currentScore: this.state.currentScore + 1 })
+				console.log("Score:" + this.state.currentScore);
+			}
+		}
+
+		this.state.selectedCards.push(id);
+		console.log("Image: " + this.state.selectedCards);
+		this.shuffleCards();
 	}
+
 
 	//render
 	render() {
-		// this.shuffleCards();
 		return (
 			<Wrapper>
 				<Navbar></Navbar>
@@ -38,14 +59,14 @@ class App extends Component {
 					{this.state.xmen.map(mutant => (
 						<Card
 							shuffleCards={this.shuffleCards}
+							selectCard={this.selectCard}
 							id={mutant.id}
 							key={mutant.id}
 							name={mutant.name}
-							image={mutant.imageWeb}
+							image={mutant.image}
 						/>
 					))}
 				</div>
-				{/* <Card></Card> */}
 				<Footer></Footer>
 			</Wrapper>
 		);
--- clicky-x-men/src/components/cards/cards.js
+++ clicky-x-men/src/components/cards/cards.js
@@ -2,18 +2,9 @@ import React from "react";
 import "./cards.css";
 
 function Card(props) {
-	const imgStyles = {
-		cardImage: {
-			backgroundImage: {url:(props.image)}
-		}
-	}
-	console.log(props);
 	return (
-		<div className="card click-item" style={imgStyles.cardImage} onClick={() => props.shuffleCards(props.id)}>
-			{/* <div className="img-container"> */}
-				<img src={props.image} />
-				{/* <span onClick={() => props.selectCard(props.id)}></span> */}
-			{/* </div> */}
+		<div className="card click-item" onClick={() => props.selectCard(props.id)}>
+			<img src={props.image} />
 		</div>
 	)
 }
